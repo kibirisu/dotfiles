7f455e24cb34715102f4699b2e06f785e6b1e943
kibirisu <szgie@proton.me>
2024-07-02 18:29:32 +0200

    Initial commit

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,18 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.o.clipboard = "unnamedplus"
+
+-- nvim.tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("lazy").setup("plugins")
+require("telescope").load_extension "file_browser"
