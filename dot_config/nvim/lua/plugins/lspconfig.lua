302cc61f9eda0cbefaf227db169d133f353a15ec
kibirisu <szgie@proton.me>
2024-07-03 18:13:41 +0200

    cmp works and smooth scrolling added to Neovim

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -5,6 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     "hrsh7th/nvim-cmp",
     "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip",
   },
   config = function()
     local capabilities = vim.tbl_deep_extend("force", vim.lsp.protocol.make_client_capabilities(), require('cmp_nvim_lsp').default_capabilities())
@@ -24,5 +25,6 @@ return {
         },
       },
     })
+    require("cmp").setup()
   end
 }
--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,3 +1,7 @@
 return {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {"karb94/neoscroll.nvim",
+  config = function ()
+    require('neoscroll').setup({})
+  end},
 }
